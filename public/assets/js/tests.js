47bcdafc6a6449b9dc5f6fda8ec47db653cbb994
Danjie3000 <90595974+Danjie3000@users.noreply.github.com>
2023-03-22 22:06:25 +0100

    a lot of info added

--- public/assets/js/tests.js
+++ public/assets/js/tests.js
@@ -26,3 +26,34 @@ function calculator(num1, num2, callback) {
   }
   
   calculator(5, 5);
+
+
+const dudes = [
+    { 
+        name: 'Danjie', 
+        age: 33 ,
+        occupation: 'being cool'
+    },
+
+    { 
+        name: 'John', 
+        age: 73,
+        occupation: 'being old'
+    },
+
+    { 
+        name: 'Jane', 
+        age: 25,
+        occupation: 'being young'
+    }
+];
+
+const oldDudes = dudes.filter(dudes => {
+    return dudes.age >= 30; // Will return Danjie and John.
+}).map(dudes => { // Since it's still an array, we can map it.
+    return dudes.name; // Instead of returning the whole object, we just return the name.
+});
+
+const totalAge = dudes.reduce((total, dudes) => { // With reduce you can return anything. 
+    return total + dudes.age; // It's normally used for numbers and it doesn't even have to be an array.
+}, 0); // 0 here is because we want to start from 0 in the array, this means it starts from Danjie. 
