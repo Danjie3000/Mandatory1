22aca029819e3e16df8cee905babca2687477c68
Danjie3000 <90595974+Danjie3000@users.noreply.github.com>
2023-03-28 20:56:31 +0200

    contact funktion added og started på email funktion test

--- app.js
+++ app.js
@@ -6,75 +6,106 @@ import path from "path";
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 import templateEngine from "./util/templateEngine.js";
 
+// Frontpage.
+const frontpage = templateEngine.readPage('./public/pages/frontpage/frontpage.html');
+const frontpagePage = templateEngine.renderPage(frontpage, {
+tabTitle : "Rock'n'Roll",
+});
+
+// intro.
+const intro = templateEngine.readPage('./public/pages/infoPages/intro.html');
+const introPage = templateEngine.renderPage(intro, {
+tabTitle : "Rock'n'Roll",
+});
+
+// first.
+const first = templateEngine.readPage('./public/pages/infoPages/first.html');
+const firstPage = templateEngine.renderPage(first, {
+tabTitle : "Rock'n'Roll",
+});
+
+// second.
+const second = templateEngine.readPage('./public/pages/infoPages/second.html');
+const secondPage = templateEngine.renderPage(second, {
+tabTitle : "Rock'n'Roll",
+});
+
+// third.
+const third = templateEngine.readPage('./public/pages/infoPages/third.html');
+const thirdPage = templateEngine.renderPage(third, {
+tabTitle : "Rock'n'Roll",
+});
+
+// fourth.
+const fourth = templateEngine.readPage('./public/pages/infoPages/fourth.html');
+const fourthPage = templateEngine.renderPage(fourth, {
+tabTitle : "Rock'n'Roll",
+});
+
+// fifth.
+const fifth = templateEngine.readPage('./public/pages/infoPages/fifth.html');
+const fifthPage = templateEngine.renderPage(fifth, {
+tabTitle : "Rock'n'Roll",
+});
+
+// sixth.
+const sixth = templateEngine.readPage('./public/pages/infoPages/sixth.html');
+const sixthPage = templateEngine.renderPage(sixth, {
+tabTitle : "Rock'n'Roll",
+});
+
+// contact.
+const contact = templateEngine.readPage('./public/pages/infoPages/contact.html');
+const contactPage = templateEngine.renderPage(contact, {
+tabTitle : "Rock'n'Roll",
+});
+
 app.get('/', (req, res) => {
-    const frontpage = templateEngine.readPage('./public/pages/frontpage/frontpage.html');
-    const frontpagePage = templateEngine.renderPage(frontpage, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(frontpagePage);
 });
 
 app.get('/intro', (req, res) => {
-    const intro = templateEngine.readPage('./public/pages/infoPages/intro.html');
-    const introPage = templateEngine.renderPage(intro, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(introPage);
 });
 
 app.get('/first', (req, res) => {
-    const first = templateEngine.readPage('./public/pages/infoPages/first.html');
-    const firstPage = templateEngine.renderPage(first, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(firstPage);
 });
 
 app.get('/second', (req, res) => {
-    const second = templateEngine.readPage('./public/pages/infoPages/second.html');
-    const secondPage = templateEngine.renderPage(second, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(secondPage);
 });
 
 app.get('/third', (req, res) => {
-    const third = templateEngine.readPage('./public/pages/infoPages/third.html');
-    const thirdPage = templateEngine.renderPage(third, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(thirdPage);
 });
 
 app.get('/fourth', (req, res) => {
-    const fourth = templateEngine.readPage('./public/pages/infoPages/fourth.html');
-    const fourthPage = templateEngine.renderPage(fourth, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(fourthPage);
 });
 
 app.get('/fifth', (req, res) => {
-    const fifth = templateEngine.readPage('./public/pages/infoPages/fifth.html');
-    const fifthPage = templateEngine.renderPage(fifth, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(fifthPage);
 });
 
 app.get('/sixth', (req, res) => {
-    const sixth = templateEngine.readPage('./public/pages/infoPages/sixth.html');
-    const sixthPage = templateEngine.renderPage(sixth, {
-    tabTitle : "Rock'n'Roll",
-    });
     res.send(sixthPage);
+}); 
+
+app.get('/contact', (req, res) => {
+    res.send(contactPage);
 }); 
 
 // API
 
+app.post("/api/contact", (req, res) => {
+    console.log(req.body);
+    res.redirect("/");
+});
 
 // API
 
